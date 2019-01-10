df9cde1503984859750a98cc141ae49a64537b86
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-10 10:46:57 +0900

    Crawling

--- index.js
+++
@@ -1,8 +0,0 @@
-var cheerio = require('cheerio');
-var request = require('request');
-
-var url = 'http://www.melon.com/chart/';
- 
-request(url, function(error, response, html){
-    var $ = cheerio.load(html);
-})
