6042361f9d40803f8dd6f363c876e6a14c9c21db
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-10 11:32:08 +0900

    Crawling

--- web_download.js
+++ web_download.js
@@ -1,28 +1,7 @@
-// // url에 있는 파일을 savepath에 다운로드한다
-
-// // 다운로드할 URL을 지정
-// var url = "http://jpub.tistory.com/";
-// // 저장할 위치를 지정
-// var savepath = "test.html";
-
-// // 사용 모듈 정의
-// var http = require('http');     // HTTP 모듈
-// var fs = require('fs');         // 파일 처리 관련 모듈
-
-// // 출력 지정
-// var outfile = fs.createWriteStream(savepath);
-
-// // 비동기로 URL의 파일 다운로드
-// http.get(url, function(res){
-//     res.pipe(outfile);
-//     res.on('end', function(){
-//         outfile.close();
-//         console.log("ok");
-//     });
-// });
-
 download(
+    // 다운로드할 URL을 지정
     "http://jpub.tistory.com/539",
+    // 저장할 위치를 지정
     "spring.html",
     function(){
         console.log("ok, spring.");
@@ -38,12 +17,15 @@ download(
 );
 
 // url의 파일을 savepath에 다운로드하는 함수
-
 function download(url, savepath, callback){
-    var http = require('http');
-    var fs = require('fs');
+    // 사용 모듈 정의
+    var http = require('http');     // HTTP 모듈
+    var fs = require('fs');         // 파일 처리 관련 모듈
+
+    // 출력 지정
     var outfile = fs.createWriteStream(savepath);
 
+    // 비동기로 URL의 파일 다운로드
     var req = http.get(url, function(res){
         res.pipe(outfile);
         res.on('end', function(){
