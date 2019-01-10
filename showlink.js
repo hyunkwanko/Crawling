0dc6bacc4ca5503020db790479b25d6002c72ec4
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-10 14:32:23 +0900

    Crawling

---
+++ showlink.js
@@ -0,0 +1,18 @@
+// 모듈 코드
+var client = require('cheerio-httpcli');
+
+// 다운로드
+var url = "http://jpub.tistory.com";
+var param = {};
+
+client.fetch(url, param, function(err, $, res){
+    // 에러 체크
+    if (err) { console.log("Error: ", err); return; }
+
+    // 링크를 추출하여 표시
+    $("a").each(function(idx){ // idx -> object들을 순차적으로 가져온다.
+        var text = $(this).text();
+        var href = $(this).attr('href');
+        console.log(text + ":" + href);
+    });
+});
