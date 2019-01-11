cf5926190e0802e7952eeb38cc5a01c886c6cd8d
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-11 10:23:59 +0900

    Crawling

--- download-node-request.js
+++ download-node-request.js
@@ -4,16 +4,34 @@ var request = require('request');
 var fs = require('fs');
 var urlType = require('url');
 
-// console.log(__dirname); // 현재 디렉토리 경로
-// console.log(__filename); // 현재 파일까지의 경로
+// console.log(__dirname);      // 현재 디렉토리 경로
+// console.log(__filename);     // 현재 파일까지의 경로
 
 
 // 저장할 디렉터리가 없으면 생성
 var savedir = __dirname + "/img";
+if (!fs.existsSync(savedir)){
+    fs.mkdirSync(savedir);
+}
 
-// // URL 지정
-// var url = "http://jpub.tistory.com/";
-// var savepath = "test.html";
+// URL 지정
+var url = "https://ko.wikipedia.org/wiki/" + encodeURIComponent("강아지");
+var param = {};
 
-// // 다운로드
-// request(url).pipe(fs.createWriteStream(savepath));
+// HTML 파일 획득
+client.fetch(url, param, function(err, $, res){
+    if (err) { console.log("error"); return; }
+    // img 링크 추출하여 각 링크에 대해 함수 수행
+    $("img").each(function(idx){
+        var src = $(this).attr('src');
+        // 상대 경로를 절대 경로로 변환
+        src = urlType.resolve(url, src);
+
+        // 저장 파일 이름 결정
+        var fname = urlType.parse(src).pathname;
+        fname = savedir + "/" + fname.replace(/[^a-zA-Z0-9\.]+/g, '_');
+
+        // 다운로드
+        request(src).pipe(fs.createWriteStream(fname));
+    });
+});
