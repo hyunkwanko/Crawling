ec69b44fe4cf2d18e17274a2642cf48aa7b98233
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-11 11:47:18 +0900

    Crawling

--- getall.js
+++ getall.js
@@ -11,3 +11,28 @@ var path = require('path');
 var LINK_LEVEL = 3;
 // 기준 URL 지정
 var TARGET_URL = "https://nodejs.org/docs/latest-v0.10.x/api/";
+var list = {};
+
+// 메인 처리
+downloadRec(TARGET_URL, 0);
+
+// 지정 URL을 최대 level 단계가지 다운로드
+function downloadRec(url, level){
+    // 최대 level 확인
+    if (level >= LINK_LEVEL)
+        return;
+    
+    // 이미 다운받은 사이트는 무시
+    if (list[url])
+        return;
+    list[url] = true;
+
+    // 외부 페이지는 무시
+    var us = TARGET_URL.split("/");
+    us.pop();
+    var base = us.join("/");
+    if (url.indexOf(base) < 0)
+        return;
+
+    // HTML을 취득
+}
