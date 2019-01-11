a38ca89b79817507a3a03e546f103ccb79fe4a0f
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-11 10:12:51 +0900

    Crawling

--- download-node-request.js
+++ download-node-request.js
@@ -1,10 +1,19 @@
 // 모듈 로드
+var client = require('cheerio-httpcli');
 var request = require('request');
 var fs = require('fs');
+var urlType = require('url');
 
-// URL 지정
-var url = "http://jpub.tistory.com/";
-var savepath = "test.html";
+// console.log(__dirname); // 현재 디렉토리 경로
+// console.log(__filename); // 현재 파일까지의 경로
 
-// 다운로드
-request(url).pipe(fs.createWriteStream(savepath));
+
+// 저장할 디렉터리가 없으면 생성
+var savedir = __dirname + "/img";
+
+// // URL 지정
+// var url = "http://jpub.tistory.com/";
+// var savepath = "test.html";
+
+// // 다운로드
+// request(url).pipe(fs.createWriteStream(savepath));
