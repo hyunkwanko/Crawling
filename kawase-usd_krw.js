5752df8f233719df83d75ff777e06aeccaad6c54
hyunkwanko <cjdshrwkd11@gmail.com>
2019-01-14 16:54:58 +0900

    Crawling

--- kawase-usd_krw.js
+++ kawase-usd_krw.js
@@ -15,14 +15,17 @@ request(API, function(err, response, body){
 
     // JSON을 JS 객체로 변환
     var r = JSON.parse(body);
+    // console.log(r);
     var krw = r["KRW"];
 
     // 환율을 파일에 저장(파일명에는 날짜 표기)
     var t = new Date();
     var fname = "USD_KRW_" +
         t.getFullYear() + "-" + (t.getMonth() + 1) +
-        "-" + t.getDay() + ".txt";
-    var text = "1usd = " + krw + "krw";
-    // console.log(text);
-    // fs.writeFile(fname, text);
+        "-" + t.getDate() + ".txt";
+    var text = "1 USD = " + krw + " KRW";
+    console.log(fname);
+    fs.writeFile(fname, text, function(){
+        console.log("Success");
+    });
 });
